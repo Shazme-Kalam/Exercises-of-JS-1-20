833917e333e037c98ba94ea20ef1ed0fdcc9db95
Shazme-Kalam <miss.shazmekalam66@gmail.com>
2023-09-23 19:56:47 -0700

    solve alert

---
+++ alert.js
@@ -0,0 +1,26 @@
+//## 1 Write a program to display alert with your name and class on next line.(use line break)
+//Solution
+// alert("My name is SHAZME \n I read in class WMA-Batch10");
+// alert(`My name is Shazme Kalam
+//  My class is WMA-F0030`);
+//## 2 Declare a variable birthYear and assign your birthyear in this variable and alert the type of the variable 
+//like this 'type of birth year is number'
+//Solution
+// var birthYear = '1995';
+// alert("My birth year is = " + birthYear );
+/*##3 Display in the browser 
+ a. A heading stating “Rules for naming JS variables”
+  b. Variable names can only contain ______, ______,
+  c. ______ and ______.
+  d. For example $my_1stVariable.
+  e. Variables must begin with a ______, ______ or
+  f. _____. For example $name, _name or name
+  g. Variable names are case _________
+  h. Variable names should not be JS _________*/
+  //Solution
+//   alert("Rules for naming JS variables \n lowercase (a-z),\n upperCase (A-Z),\n alphanumeric (name12),\n (_use) underscores. ");
+//   document.write(" <b>Rules for naming JS variables</b> <br /> a)lowercase(a-z) , b)upperCase(A-Z) , c)alphanumeric(name12) , e:(_use)underscores. ");
+//   document.write(" g. Variable names are case SENSITIVE");
+//   document.write(" h. Variable names should not be JS in DIGIT(0123..)");
+//   document.write(" e. Variables must begin with a LOWERCASE(name), (_use)underscoresor, or with $sign");
+
---
+++ prompt.js
@@ -0,0 +1,29 @@
+// ## 4 Write a program that returns your age in days. (take age as an input)
+//Solution
+// var age = +prompt('Enter your Age', 'type your age only in number');
+// var days = age * '365';
+// console.log("your age is " + days + " days");
+
+//## 5 Create a program that takes a number as an input, increments the number by +1 and
+ //returns the result. (take number as an input)
+ //Solution
+//  var num = +prompt('Enter any number to gain 1+ number ', 'number');
+//  var _num =  ++num ;
+//  console.log("Entered number returns 1+, Result = " + _num);
+
+//## 6 Create a program that takes a number as an input, decrements the number by -1  
+//and returns the result.
+//Solution
+// var num = +prompt('Enter any number to loss 1 number ', 'number');
+// var _num =  --num ; 
+// console.log("Entered number -1, Result = " + _num);
+
+//## 7 Create a function that takes a number and return square of a number
+//Solution
+// var num = +prompt('Enter any number to loss 1 number ', 'number');
+// var _num =  --num ; 
+// console.log("Entered number -1, Result = " + _num);
+
+//## 7 Create a function that takes a number and return square of a number
+//Solution
+
