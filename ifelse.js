98cc6ec14b8b1986c787a7e1f1e48b0d7e930455
Shazme-Kalam <miss.shazmekalam66@gmail.com>
2023-10-22 01:41:10 -0700

    solve some question

--- ifelse.js
+++ ifelse.js
@@ -24,10 +24,82 @@
 //  welcome the user like this: “Welcome to city of lights” if enter lahore,
 //  display "welcome to historical city" if Quetta display message "welcome to cold city"
 //Solution
-// var city = prompt('Enter the city name ',city);
-// if(city === "karachi"){
+// var city = prompt('Enter the city name ', city);
+// if (city === "karachi") {
+//     alert("Welcome to city of lights")
 //     console.log("Welcome to city of lights")
-// } else if(city === "lahore"){
+// } else if (city === "lahore") {
 //     console.log("welcome to historical city")
+// } else if (city === "quetta") {
+//     console.log("welcome to cold city")
+// };
+
+//## 12 Write a program to create a calculator for +,-,*, / & % using if statements.
+// Take the following input:  a. First number  b. Second number 
+// c. Operation (+, -, *, /, %)    Compute & show the calculated result to user.
+ // Solution
+//   var num = +prompt('Enter the first number to calculate');
+//   var operator = prompt('Enter any operation signs + - * % / to find your answer',);
+//   var num1 = +prompt('Enter the second number to calculate');
+ 
+//   if(operator == "+"){
+//   result=num+num1}
+//     if(operator == "-"){
+//     result=num-num1}
+//       if(operator == "*"){
+//         result=num*num1}
+//         if(operator == "/"){
+//             result=num/num1}
+//             if(operator == "%"){
+//                 result=num%num1}
+//       console.log("your result is",result)
+  
+// ## 13 Write a program to take user salary as an input  if salary is less than 60000 than
+//  print "you are a junior developer" if salary is higher or equal to than 60000 
+//  "You are a web developer"  if higher equal to 100000 then print message 
+//  "you are a software engineer"
+
+//Solution
+// var salary = +prompt("Enter your salary amount in number form ");
+// if(salary > 20000 && salary < 60000){
+// document.write("You are a Junior Developer")
+// } else if(salary >= 60000 && salary <= 99999){
+// document.write("You are a Web Developer")
+    
+// } else if(salary >= 100000){
+//     document.write("You are a Software Engineer")
+
+// } else if(salary <= 20000){
+//     document.write("You are in Internship")
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
